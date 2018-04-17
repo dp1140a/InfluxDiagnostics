ed9f6a4d0ab22bdeda1f26b085ec947198b9e725
Dave Patton <dave@mymobile.info>
2018-04-16 17:04:55 -0700

    Major revisions to script execution and outputs Added in qprof capabilities Revised README

--- countsSeriesAndMeasurements.sh
+++ countsSeriesAndMeasurements.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+#countsSeriesAndMeasurements.sh
 
 # What: Series and measurement counts from the last 24 hours, grouped by hour, database, and hostname.
 # Why: This allows us to see the current series and measurement count, as well as how much the series count has grown per hour over the last 24 hours
+echo -e "**************************************"
+echo -e countsSeriesAndMeasurements.sh
+echo -e "**************************************"
 
 INFLUXDB_USER="foo"
 INFLUXDB_PASSWORD="bar"
+OUT_DIR="output"
 
 while [[ $# -gt 1 ]]
 do
@@ -12,11 +17,15 @@ key="$1"
 
 case $key in
     -u|--username)
-    $INFLUXDB_USER="$2"
+    INFLUXDB_USER="$2"
     shift # past argument
     ;;
     -p|--password)
-    $INFLUXDB_PASSWORD="$2"
+    INFLUXDB_PASSWORD="$2"
+    shift # past argument
+    ;;
+    -o|--outdir)
+    OUT_DIR="$2"
     shift # past argument
     ;;
     *)
@@ -26,11 +35,8 @@ esac
 shift # past argument or value
 done
 
-influx -username $INFLUXDB_USER -password $INFLUXDB_PASSWORD -execute 'SELECT last(numMeasurements) as last_measurement, last(numSeries) as last_series FROM "_internal"."monitor"."database" WHERE time > now() - 24h GROUP BY time(1h),"database","hostname" fill(0)
-' > countsSeriesAndMeasurements.txt
+echo -e "Running query"
+# echo $INFLUXDB_USER : $INFLUXDB_PASSWORD
+influx -username $INFLUXDB_USER -password $INFLUXDB_PASSWORD -database '_internal' -execute 'SELECT last(numMeasurements) as last_measurement, last(numSeries) as last_series FROM "_internal"."monitor"."database" WHERE time > now() - 24h GROUP BY time(1h),"database","hostname" fill(0)' > $OUT_DIR/countsSeriesAndMeasurements.txt
 
-#ZIP it all up and send it
-#tar -czf ${BASE_NAME}.tar.gz $OUT_DIR
-
-echo "Done!!"
-echo "Please send the tar file to Influxdata support"
+echo "Done."
--- readWriteResponse.sh
+++ readWriteResponse.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# readWriteResponse.sh
 
 # What: Hinted-handoff queue size from the last 24 hours, grouped by hour, and all tags.
 # Why: This will tell us how large the hinted-handoff queue was for each hour, broken out by target server.
+echo -e "**************************************"
+echo -e readWriteResponse.sh
+echo -e "**************************************"
 
 INFLUXDB_USER="foo"
 INFLUXDB_PASSWORD="bar"
@@ -12,11 +16,15 @@ key="$1"
 
 case $key in
     -u|--username)
-    $INFLUXDB_USER="$2"
+    INFLUXDB_USER="$2"
     shift # past argument
     ;;
     -p|--password)
-    $INFLUXDB_PASSWORD="$2"
+    INFLUXDB_PASSWORD="$2"
+    shift # past argument
+    ;;
+    -o|--outdir)
+    OUT_DIR="$2"
     shift # past argument
     ;;
     *)
@@ -26,11 +34,11 @@ esac
 shift # past argument or value
 done
 
-influx -username $INFLUXDB_USER -password $INFLUXDB_PASSWORD -execute 'SELECT non_negative_derivative(max("queryReqDurationNs")) / non_negative_derivative(max("queryReq")) / 1000000 as queryReqDurationMs FROM "_internal"."monitor"."httpd" WHERE time > now() - 24h GROUP BY time(1h),* fill(0)' >> readWriteResponse.txt
-influx -username $INFLUXDB_USER -password $INFLUXDB_PASSWORD -execute 'SELECT non_negative_derivative(max("writeReqDurationNs")) / non_negative_derivative(max("writeReq")) / 1000000 as writeReqDurationMs FROM "_internal"."monitor"."httpd" WHERE time > now() - 24h GROUP BY time(1h),* fill(0)' >> readWriteResponse.txt
+echo -e "Running query"
+influx -username $INFLUXDB_USER -password $INFLUXDB_PASSWORD -execute 'SELECT non_negative_derivative(max("queryReqDurationNs")) / non_negative_derivative(max("queryReq")) / 1000000 as queryReqDurationMs FROM "_internal"."monitor"."httpd" WHERE time > now() - 24h GROUP BY time(1h),* fill(0)' >> $OUT_DIR/readWriteResponse.txt
+influx -username $INFLUXDB_USER -password $INFLUXDB_PASSWORD -execute 'SELECT non_negative_derivative(max("writeReqDurationNs")) / non_negative_derivative(max("writeReq")) / 1000000 as writeReqDurationMs FROM "_internal"."monitor"."httpd" WHERE time > now() - 24h GROUP BY time(1h),* fill(0)' >> $OUT_DIR/readWriteResponse.txt
 
 #ZIP it all up and send it
 #tar -czf ${BASE_NAME}.tar.gz $OUT_DIR
 
 echo "Done!!"
-echo "Please send the tar file to Influxdata support"
